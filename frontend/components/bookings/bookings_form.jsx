e4b3bfa922b456b3fa3092fa2d1c203bac729a55
youknowhu <kimberly.hu@gmail.com>
2018-04-08 16:17:00 -0700

    Download React DayPicker and DateFormat

--- frontend/components/bookings/bookings_form.jsx
+++ frontend/components/bookings/bookings_form.jsx
@@ -1,5 +1,9 @@
 import React from 'react';
 import { withRouter } from 'react-router-dom';
+import DayPickerInput from 'react-day-picker/DayPickerInput';
+import format from 'date-fns/format';
+import parse from 'date-fns/parse';
+
 
 
 class BookingsForm extends React.Component {
@@ -25,17 +29,28 @@ class BookingsForm extends React.Component {
 
 
   render() {
-    console.log(this.props)
     const { bookings, currentUser, listing, booking } = this.props;
+    const dateSettings = {
+      clickUnselectsDay: true,
+      placeholder: "Select date",
+      format: 'M/D/YYYY',
+      formatDate: format,
+      parseDate: parse,
+    }
+
+
 
     if (!listing) {
       return (<div> </div>)
     } else {
       return (
-        <form className="booking-form">
-          <div className='booking-header'>
+        <form className="booking-form" onSubmit={this.handleSubmit}>
+          <div className="booking-header">
             <h2>${listing.dailyCost}</h2>
             <button>Book Site</button>
+          </div>
+          <div className="booking-params">
+              <DayPickerInput {...dateSettings} />
           </div>
         </form>
       )
