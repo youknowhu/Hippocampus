7dbe4f7ecfc5de2be21fbd09df28b3731e1090df
youknowhu <kimberly.hu@gmail.com>
2018-04-15 10:33:56 -0700

    updated footer links and styling

--- frontend/components/footer.jsx
+++ frontend/components/footer.jsx
@@ -5,42 +5,43 @@ export default () => {
     <footer>
       <section className="footer-main">
         <div className="footer-container">
-          <div className="row">
-            <div className="footer-col left">
+          <div className="row left footer-col">
+            <div className="footer-left-col">
               <h5>Hippocampus is everywhere you want to camp.</h5>
-              <p>Discover unique experiences on ranches, nature preserves, farms, vineyards, and public campgrounds across the U.S. Book tent camping, treehouses, cabins, yurts, primitive backcountry sites, car camping, airstreams, tiny houses, RV camping, glamping tents and more. </p>
+              <p>Discover unique experiences on ranches, nature preserves,
+              farms, vineyards, and public campgrounds across the U.S. Book
+              tent camping, treehouses, cabins, yurts, primitive backcountry
+              sites, car camping, airstreams, tiny houses, RV camping, glamping
+              tents and more. </p>
             </div>
-            <div>
-              <div className="row">
-                <div className="footer-col">
-                  <h5>Browse</h5>
+          </div>
+          <div className="row right">
+            <div className="footer-col">
+              <h5>Browse</h5>
+              <ul className="list-unstyled">
+                <li><a href="https://github.com/youknowhu">Host campers</a></li>
+                <li><a href="https://github.com/youknowhu">Discover Camps</a></li>
+                <li><a href="https://github.com/youknowhu">Gift Cards</a></li>
+                <li><a href="https://github.com/youknowhu">Contact</a></li>
+              </ul>
+            </div>
+            <div className="footer-col">
+              <h5>About</h5>
+              <ul className="list-unstyled">
+                <li><a href="https://github.com/youknowhu">Journal</a></li>
+                <li><a href="https://github.com/youknowhu">My Story</a></li>
+                <li><a href="https://github.com/youknowhu">The Team</a></li>
+                <li><a href="https://github.com/youknowhu">Careers</a></li>
+              </ul>
+            </div>
+            <div className="footer-col">
+              <h5>Social</h5>
                   <ul className="list-unstyled">
-                    <li><a href="https://github.com/youknowhu">Host campers</a></li>
-                    <li><a href="https://github.com/youknowhu">Discover Camps</a></li>
-                    <li><a href="https://github.com/youknowhu">Gift Cards</a></li>
-                    <li><a href="https://github.com/youknowhu">Contact</a></li>
+                    <li><a href="https://www.facebook.com/kimmyhu"><span className="icon fa fa-facebook-official"></span>  Facebook</a></li>
+                    <li><a href="https://www.instagram.com/youknowhu/"><span className="icon fa fa-instagram"></span>  Instagram</a></li>
+                    <li><a href="https://github.com/youknowhu"> <i className="fa fa-github"></i>  Github</a></li>
+                    <li><a href="https://www.linkedin.com/in/kimberly-hu/"> <i className="fa fa-linkedin"></i>  LinkedIn</a></li>
                   </ul>
-                </div>
-                <div className="footer-col">
-                  <h5>About</h5>
-                  <ul className="list-unstyled">
-                    <li><a href="https://github.com/youknowhu">Journal</a></li>
-                    <li><a href="https://github.com/youknowhu">Our Story</a></li>
-                    <li><a href="https://github.com/youknowhu">The Team</a></li>
-                    <li><a href="https://github.com/youknowhu">Careers</a></li>
-                  </ul>
-                </div>
-                <div className="footer-col">
-                  <h5>Social</h5>
-                      <ul className="list-unstyled">
-                        <li><a href="https://github.com/youknowhu"><span className="icon fa fa-facebook-official"></span> Facebook</a></li>
-                        <li><a  href="https://github.com/youknowhu"><span className="icon fa fa-twitter"></span> Twitter</a></li>
-                        <li><a  href="https://github.com/youknowhu"><span className="icon fa fa-instagram"></span> Instagram</a></li>
-                        <li><a  href="https://github.com/youknowhu"><span className="icon fa fa-pinterest-p"></span> Pinterest</a></li>
-                        <li><a href="https://github.com/youknowhu"><span className="icon fa fa-spotify"></span> Spotify</a></li>
-                      </ul>
-                </div>
-              </div>
             </div>
           </div>
         </div>
