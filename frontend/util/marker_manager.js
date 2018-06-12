726ff6ccf281562b07ffcdc324cc4e6e4297efa8
youknowhu <kimberly.hu@gmail.com>
2018-06-12 08:48:44 -0700

    update infowindow

--- frontend/util/marker_manager.js
+++ frontend/util/marker_manager.js
@@ -16,26 +16,32 @@ class MarkerManager {
   updateMarkers(listings, geolocation, mapBounds){
     const listingsObj = {};
 
-    this.clearMarkers(listingsObj)
+    this.clearMarkers(listingsObj);
     this.bounds = new google.maps.LatLngBounds();
 
     listings.forEach(listing => listingsObj[listing.id] = listing);
 
     listings
       .filter(listing => !this.markers[listing.id])
-      .forEach(newListing => this.createMarkerFromListing(newListing))
+      .forEach(newListing => this.createMarkerFromListing(newListing));
 
+    //if user entered geolocation, map will be centered around geolocation.
     if (geolocation.length > 0) {
-      return
+      return;
+
+    //if exactly one listing, center and set zoom around single location.
     } else if (listings.length === 1) {
       this.map.setZoom(7);
       this.map.setCenter({lat: listings[0].lat, lng: listings[0].lng });
+
+    //otherwise, reframe map around bounds set by all markers placed on the map.
     } else if (listings.length > 0) {
       this.map.fitBounds(this.bounds);
     }
   }
 
   createMarkerFromListing(listing) {
+    console.log(listing);
     const icon = 'https://res.cloudinary.com/deor0br3s/image/upload/c_scale,w_100/v1523399623/Hippo_Marker_3.png'
     const position = new google.maps.LatLng(listing.lat, listing.lng);
     const marker = new google.maps.Marker({
@@ -47,31 +53,29 @@ class MarkerManager {
 
     const infowindow = new google.maps.InfoWindow({
       content:
-      `<div className="infowindow"
-        style="padding-left: 20px;"
-        >
-        <h2 className="infowindow-title"
-          style="font-weight: bold;
-          font-size: 14px;
-          text-align: center;
-          color: #777777;
-          padding-top: 5px;
-          "> ${listing.title} </h2>
+      `<div class="infowindow">
+        <a href="/#/listings/${listing.id}" style="display: flex;">
+          <img class="infowindow-image" src="${listing.iconUrl}"/>
+          <div>
+            <h2 class="infowindow-title"> ${listing.title} </h2>
+            <p>$${listing.dailyCost}/night</p>
+          </div>
+        </a>
       </div>`,
-      maxWidth: 400,
+      maxWidth: 300,
     });
 
     marker.addListener('mouseover', () => {
-      infowindow.open(this.map, marker)
+      infowindow.open(this.map, marker);
     });
 
     marker.addListener('mouseout', () => {
-      infowindow.close(this.map, marker)
+      infowindow.close(this.map, marker);
     });
 
-    marker.addListener('click', () => {
-      s
-    });
+    // marker.addListener('click', () => {
+    //   infowindow.open(this.map, marker);
+    // });
 
 
     this.markers[marker.listingId] = marker;
